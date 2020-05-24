4a7cb2fc07ec7960643467ed01989d30d6c79946
jackkitte <jackkitte735@gmail.com>
2020-05-24 12:28:22 +0900

    前ページ(Previous)ボタンの実装

--- src/App.js
+++ src/App.js
@@ -34,6 +34,15 @@ class App extends Component {
     event.preventDefault();
   }
 
+  goPrevious(search) {
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor
+    })
+  }
+
   goNext(search) {
     this.setState({
       first: PER_PAGE,
@@ -79,6 +88,16 @@ class App extends Component {
                       })
                     }
                   </ul>
+                  {
+                    search.pageInfo.hasPreviousPage === true ?
+                      <button
+                        onClick={this.goPrevious.bind(this, search)}
+                      >
+                        Previous
+                      </button>
+                      :
+                      null
+                  }
                   {
                     search.pageInfo.hasNextPage === true ?
                       <button
